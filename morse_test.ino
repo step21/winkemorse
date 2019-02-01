c05cf4d969ea0e27b4cfcdcb8b7f2ee6589346c8
step21 <step21@devtal.de>
2019-02-01 15:30:35 +0100

    morsing string successfully in one fixed colour

--- morse_test.ino
+++ morse_test.ino
@@ -1,14 +1,21 @@
 #include <morselib.h>
 
-morselib morselib(10); // specifies pin
+#define BLUE 4
+#define GREEN 12
+#define RED 14
 
+morselib morselib(RED); // specifies pin
+
+// BLUE, GREEN
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.print("Setup code run."); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  morselib.sendString("hallo");
+  morselib.sendString("hallo dies ist ein test. bitte helfen sie mir. Dies ist Proviant Limonade Gmbh Wicküler Bayreuther Dev Tal Test Overflow Test hallo dies ist ein test. bitte helfen sie mir. Dies ist Proviant Limonade Gmbh Wicküler Bayreuther Dev Tal Test Overflow Test");
+  Serial.print("String sent.");
   delay(3000);
+  Serial.print("Waiting.");
 }
